e16e9394c82d97cba48bef069a58ce1510b0c97c
Arunabh Saikia <developer.ariyanas@gmail.com>
2017-07-23 04:15:45 +0530

    Completed all the functionality of the application

--- note-app/app.js
+++ note-app/app.js
@@ -1,20 +1,75 @@
-const fs = require('fs');
 const  _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
+const argv = yargs.command('add','Add a new Note',{
+  tittle:{
+    describe: 'Title of the note',
+    demand: true,
+    alias: 't'
+  },
+  description:{
+    describe: 'Description of the note',
+    demand: true,
+    alias: 'd'
+  }
+}).command('remove','Removes a note',{
+  tittle:{
+    describe: 'Title of the note',
+    demand: true,
+    alias: 't'
+  }
+}).command('read','Reads a note',{
+  tittle:{
+    describe: 'Title of the note',
+    demand: true,
+    alias: 't'
+  }
+}).command('edit','Edit a note\'s description',{
+  tittle:{
+    describe: 'Title of the note to be modified',
+    demand: true,
+    alias: 't'
+  },
+  edit:{
+    describe: 'Description to be modified',
+    demand: true,
+    alias: 'e'
+  }
+}).command('list','Lists all the notes').help().argv;
 var command = argv._[0];
 
 if(command == 'add'){
-  notes.addNote(argv.title,argv.description);
+  var newNote = notes.addNote(argv.title,argv.description);
+
+  if(newNote){
+    console.log('Note added successfully !');
+    console.log('-------------------------');
+    console.log(`Title : ${newNote.title}`);
+    console.log(`Description : ${newNote.description}`);
+  }else{
+    console.log(`Note with title : "${argv.title}" ,already exists !!!`);
+  }
 }else if(command == 'read'){
-  notes.getNote(argv.title);
+  var readNote = notes.getNote(argv.title);
+
+  if(readNote){
+    console.log('Note found !');
+    console.log('-------------------------');
+    console.log(`Title : ${readNote.title}`);
+    console.log(`Description : ${readNote.description}`);
+  }else{
+    console.log('Note not found');
+  }
 }else if(command == 'remove'){
-  notes.removeNote(argv.title);
+  var removeNote = notes.removeNote(argv.title);
+
+  var msg = removeNote ? 'Note removed successfully !!!' : 'Note not found !!!';
+
+  console.log(msg);
 }else if(command == 'edit'){
-  notes.editNote(argv.title);
+  notes.editNote(argv.title,argv.edit);
 }else if(command == 'list'){
   notes.getList();
 }else{
--- note-app/notes.js
+++ note-app/notes.js
@@ -1,22 +1,85 @@
-var addNote = (title,description) => {
+const fs = require('fs');
 
-}
+var fetchNotes = () => {
+  try{
+    var notesTxt = fs.readFileSync('my-notes.json');
+    return JSON.parse(notesTxt);
+  }catch(e){
+    return [];
+  }
+};
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('my-notes.json',JSON.stringify(notes));
+};
+
+var addNote = (title,description) => {
+  var notes = fetchNotes();
+
+  var newNote = {
+    title,
+    description
+  };
+
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if(duplicateNotes.length === 0){
+    notes.push(newNote);
+    saveNotes(notes);
+    return newNote;
+  }
+};
 
 var getList = () => {
-
-}
+  var notes = fetchNotes();
+  var i = 1;
+  notes.forEach(function(note){
+    console.log('----------------------');
+    console.log("Note : "+i);
+    console.log('----------------------');
+    console.log('Title : '+note.title);
+    console.log('Description : '+note.description);
+    console.log('----------------------');
+    i++;
+  });
+};
 
 var getNote = (title) =>{
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title === title);
 
-}
+  return filteredNotes[0];
+};
 
 var removeNote = (title) => {
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title !== title);
 
-}
+  saveNotes(filteredNotes);
 
-var editNote = (title) => {
+  return notes.length !== filteredNotes.length;
+};
 
-}
+var editNote = (title,edit) => {
+  var notes = fetchNotes();
+
+  var noteEdit = null;
+
+  var filteredNotes = notes.filter((note) => note.title === title);
+
+  if(filteredNotes.length === 0){
+    console.log('Note not found !!!')
+  }else{
+    notes.forEach((note) => {
+      if(note.title === title){
+        note.description = edit;
+      }
+    });
+
+    saveNotes(notes);
+    console.log('Note edited successfully !!!');
+  }
+};
 
 module.exports = {
   addNote,
@@ -24,4 +87,4 @@ module.exports = {
   getNote,
   removeNote,
   editNote
-}
+};
